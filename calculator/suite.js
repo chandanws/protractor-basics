1b81effc9be8345ab1e8e1dba06e1d56ad6518b2
Valentine Shemyako <valentineshemyako@rentalcars.com>
2019-08-21 23:34:49 +0300

    Verified basic addition functionality

--- calculator/suite.js
+++ calculator/suite.js
@@ -1,7 +1,25 @@
+/**
+ * End-to-end test suite for testing simple Calculator app.
+ */
 describe('Calculator App', function () {
     let homePageUrl = 'http://juliemr.github.io/protractor-demo/';
+
+    /**
+     * Verifies page title.
+     */
     it('Should Have Title', function () {
         browser.get(homePageUrl);
         expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+
+    /**
+     * Verifies basic addition functionality.
+     */
+    it('Should Sum Two Numbers', function () {
+        browser.get(homePageUrl);
+        element(by.model('first')).sendKeys(5);
+        element(by.model('second')).sendKeys(10);
+        element(by.id('gobutton')).click();
+        expect(element(by.binding('latest')).getText()).toEqual('15');
     });
 });
